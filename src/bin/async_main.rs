602ce2fe89ac3fba0e41dfb92fa0b2afcd393494
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2025-03-04 14:40:43 -0500

    button listening

--- src/bin/async_main.rs
+++ src/bin/async_main.rs
@@ -1,25 +1,35 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
+
 use alloc::string::String;
+use critical_section::Mutex;
 use embassy_executor::Spawner;
 use embassy_futures::select::{select, Either};
-use embassy_time::{Duration, Ticker};
-use esp_backtrace as _;
-use esp_hal::clock::CpuClock;
-use esp_println::println;
-use esp_wifi::esp_now::PeerInfo;
-use log::info;
+use embassy_time::{Duration, Ticker, Timer};
 use esp_alloc as _;
 use esp_backtrace as _;
-
+use esp_backtrace as _;
+use esp_hal::{
+    clock::CpuClock,
+    gpio::{Input, Io},
+    handler,
+    interrupt::InterruptConfigurable,
+    ram,
+    time::now,
+};
+use esp_println::println;
+use esp_wifi::esp_now::{PeerInfo, BROADCAST_ADDRESS};
+use log::info;
 
 extern crate alloc;
 
 const PENDENT_ADDRESS: [u8; 6] = [0x88u8, 0x88u8, 0x88u8, 0x88u8, 0x88u8, 0x88u8];
+static BUTTON: Mutex<RefCell<Option<Input>>> = Mutex::new(RefCell::new(None));
 
 #[esp_hal_embassy::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     // generator version: 0.2.2
 
     let peripherals = esp_hal::init(esp_hal::Config::default().with_cpu_clock(CpuClock::max()));
@@ -46,17 +56,23 @@ async fn main(_spawner: Spawner) {
     let mut esp_now = esp_wifi::esp_now::EspNow::new(&esp_wifi_ctrl, wifi).unwrap();
 
     println!("esp-now version {}", esp_now.version().unwrap());
-    let mut ticker = Ticker::every(Duration::from_secs(5));
+    let mut ticker = Ticker::every(Duration::from_secs(1));
 
-    // let gpio = peripherals.GPIO;
-    // gpio.enable();
-    // let pin_reg = gpio.pin(2);
-    // pin_reg.
+    // let mut last_pressed = todo!();
 
-    // Received ReceivedData { data: [72, 101, 108, 108, 111, 32, 80, 101, 101, 114], info: ReceiveInfo { src_address: [88, 207, 121, 233, 147, 52], dst_address: [96, 85, 249, 191, 255, 180], rx_control: RxControlInfo { rssi: 202, rate: 0, sig_mode: 0, mcs: 0, cwb: 0, smoothing: 0, not_sounding: 0, aggregation: 0, stbc: 0, fec_coding: 0, sgi: 0, ampdu_cnt: 0, channel: 1, secondary_channel: 0, timestamp: 2170329067, noise_floor: 160, ant: 0, sig_len: 53, rx_state: 0 } } }
-    // Received ReceivedData { data: [48, 49, 50, 51, 52, 53, 54, 55, 56, 57], info: ReceiveInfo { src_address: [88, 207, 121, 233, 147, 52], dst_address: [255, 255, 255, 255, 255, 255], rx_control: RxControlInfo { rssi: 204, rate: 0, sig_mode: 0, mcs: 0, cwb: 0, smoothing: 0, not_sounding: 0, aggregation: 0, stbc: 0, fec_coding: 0, sgi: 0, ampdu_cnt: 0, channel: 1, secondary_channel: 0, timestamp: 2170838947, noise_floor: 160, ant: 0, sig_len: 53, rx_state: 0 } } }
-    // Send hello to peer status: Ok(())
+    // let mut flash = FlashStorage::new();
+
+    // let flash_addr = 0x9000;
+
+    spawner
+        .spawn(button_listener(Input::new(
+            peripherals.GPIO2,
+            esp_hal::gpio::Pull::Down,
+        )))
+        .unwrap();
+
     loop {
+
         let res = select(ticker.next(), async {
             let r = esp_now.receive_async().await;
             println!("Received {:?}", r);
@@ -64,7 +80,8 @@ async fn main(_spawner: Spawner) {
                 "data: {}",
                 String::from_utf8(r.data().into()).unwrap_or("BAD DATA".into())
             );
-            if r.info.dst_address == PENDENT_ADDRESS {
+            if r.info.dst_address == BROADCAST_ADDRESS {
+                println!("connected to {:?}", r.info.src_address);
                 if !esp_now.peer_exists(&r.info.src_address) {
                     esp_now
                         .add_peer(PeerInfo {
@@ -76,6 +93,7 @@ async fn main(_spawner: Spawner) {
                         .unwrap();
                 }
                 let status = esp_now.send_async(&r.info.src_address, b"Hello Peer").await;
+
                 println!("Send hello to peer status: {:?}", status);
             }
         })
@@ -84,10 +102,28 @@ async fn main(_spawner: Spawner) {
         match res {
             Either::First(_) => {
                 println!("Send");
-                let status = esp_now.send_async(&PENDENT_ADDRESS, b"0123456789").await;
-                println!("Send broadcast status: {:?}", status)
+                let status = esp_now.send_async(&BROADCAST_ADDRESS, b"0123456789").await;
+                println!("Send broadcast status: {:?}", status);
             }
             Either::Second(_) => (),
         }
+    }
+}
+
+#[embassy_executor::task]
+async fn button_listener(button_pin: Input<'static>) {
+    let mut button_pressed = false;
+    let mut last_pressed = now();
+    loop {
+        if button_pin.is_high() && !button_pressed {
+            button_pressed = true;
+            last_pressed = now();
+            println!("button just pressed");
+        } else if button_pin.is_low() && button_pressed {
+            let tim = now() - last_pressed;
+            button_pressed = false;
+            println!("button released, was pressed for {}", tim);
+        }
+        Timer::after(Duration::from_millis(1)).await;
     }
 }
